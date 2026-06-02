82373393fd6625b457cf90a8121a65880e5c8b82
Aryan <carryislive3311@gmail.com>
2026-06-02 20:04:24 +0530

    fix port for render

--- server.js
+++ server.js
@@ -132,7 +132,9 @@ io.emit("onlineUsers", Array.from(userSocketMap.keys()));
     });
 
     socket.on("joinDM", async (data) => {
-        const dmRoom = [data.from, data.to].sort().join("_");
+        const from = data.from.trim().toLowerCase();
+const to = data.to.trim().toLowerCase();
+const dmRoom = [from, to].sort().join("_");
         socket.join(dmRoom);
         const messages = await Message.find({ room: dmRoom, isDM: true }).sort({ time: 1 }).limit(50);
         socket.emit("loadMessages", messages);
